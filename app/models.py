e0fbab68f47559bde1253ac76f6617ed394b5ed7
Maria N. V. Borges <m.socorro.borges.cc@gmail.com>
2026-04-27 15:19:10 -0300

    new project structure

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    description = db.Column(db.String(300))
